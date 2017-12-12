5077843df87dc6676be98a1dcd14aa854badd326
Khen Daniel <thekhenzie@gmail.com>
2017-12-12 19:38:40 +0800

    Initial commit for admin page

---
+++ admin/auth.php
@@ -0,0 +1,16 @@
+<?php
+$username = "root";
+$password = "";
+$hostname = "localhost"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+ or die("Unable to connect to MySQL");
+
+//select a database to work 
+$db = "dbhotel";
+$selected = mysql_select_db($db,$dbhandle) 
+  or die("Could not select database");
+
+
+?>
