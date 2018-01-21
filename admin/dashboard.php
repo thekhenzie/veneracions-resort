fcb5a743a5640c198f05595e951be2eda93aa1fd
Khen Daniel <thekhenzie@gmail.com>
2018-01-22 07:28:35 +0800

    Added css design

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -26,6 +26,18 @@ if (mysql_num_rows($re) > 0) {
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
+        <!-- CSS LIBRARY -->
+        <link rel="stylesheet" type="text/css" href="css/lib/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/font-lotusicon.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/magnific-popup.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/settings.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="css/lib/devStyle.css">
+    <!-- MAIN STYLE -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- Custom styles for this template -->
     <link href="css/dashboard.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
@@ -276,14 +288,17 @@ if (mysql_num_rows($re3) > 0) {
 									<form class="form-inline" method="post" action="" >
 									  <div class="form-group">
 
-										<input class="form-control datepicker" name="checkin" id="checkin" placeholder="Start Date">
+										<input class="form-control awe-calendar from" name="checkin" id="checkin" placeholder="Start Date">
 									  </div>
 									  <div class="form-group">
 
-										<input  class="form-control datepicker" name="checkout" id="checkout" placeholder="End Date">
+										<input  class="form-control awe-calendar from" name="checkout" id="checkout" placeholder="End Date">
 									  </div>
 									  <input class="btn" id="search" name="search" value="Search" onClick="return fnSearch();" style="width: 100px; background-color: #4aa3df; color: white;" type="button">
 
+<!-- 
+                                      <input type="text" name='checkIn' id='checkIn' required class="awe-calendar from" placeholder="Arrival Date"> -->
+                                <!-- <input type="text" name='checkOut' id='checkOut' required class="awe-calendar to" placeholder="Departure Date"> -->
 									</form>
 						</div>
 
@@ -516,6 +531,22 @@ if (mysql_num_rows($re) > 0) {
 		  labels: ['Value']
 		});
   </script>
-
+    <!-- LOAD JQUERY -->
+    <script type="text/javascript" src="js/lib/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lib/bootstrap-select.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;signed_in=true"></script>
+    <script type="text/javascript" src="js/lib/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="js/lib/owl.carousel.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.appear.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.countTo.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.countdown.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="js/lib/SmoothScroll.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
 
 </body></html>
